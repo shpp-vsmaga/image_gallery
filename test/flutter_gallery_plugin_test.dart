e187951d118e082d1812846184289e24945946a5
Niko Ruotsalainen <niko.ruotsalainen@wowanders.com>
2019-06-07 18:36:15 +0300

    Fix initial unit tests

--- test/flutter_gallery_plugin_test.dart
+++ test/flutter_gallery_plugin_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_gallery_plugin/flutter_gallery_plugin.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_gallery_plugin');
@@ -15,7 +15,11 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterGalleryPlugin.platformVersion, '42');
+  test('getAllImages exists', () async {
+    expect(FlutterGalleryPlugin.getAllImages, isA<Function>());
+  });
+
+  test('getImagesForPeriod exists', () async {
+    expect(FlutterGalleryPlugin.getImagesForPeriod, isA<Function>());
   });
 }
