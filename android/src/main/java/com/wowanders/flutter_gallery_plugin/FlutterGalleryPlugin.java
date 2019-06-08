4c8bb6400450b5a41c1ab80b461bfead4d3a72f2
Niko Ruotsalainen <niko.ruotsalainen@wowanders.com>
2019-06-08 18:12:31 +0300

    Reformat Java code

--- android/src/main/java/com/wowanders/flutter_gallery_plugin/FlutterGalleryPlugin.java
+++ android/src/main/java/com/wowanders/flutter_gallery_plugin/FlutterGalleryPlugin.java
@@ -3,7 +3,6 @@ package com.wowanders.flutter_gallery_plugin;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Application;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -20,8 +19,6 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -29,7 +26,9 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FlutterGalleryPlugin */
+/**
+ * FlutterGalleryPlugin
+ */
 public class FlutterGalleryPlugin implements MethodCallHandler {
   private static final String METHOD_GET_ALL = "getAllImages";
   private static final String METHOD_GET_FOR_PERIOD = "getImagesForPeriod";
@@ -58,14 +57,14 @@ public class FlutterGalleryPlugin implements MethodCallHandler {
         break;
       case METHOD_GET_FOR_PERIOD:
         long startPeriod = call.hasArgument(ARGUMENT_PERIOD_START)
-                ? (long) call.argument(ARGUMENT_PERIOD_START)
-                : 0L;
+          ? (long) call.argument(ARGUMENT_PERIOD_START)
+          : 0L;
 
         long endPeriod = call.hasArgument(ARGUMENT_PERIOD_END)
-                ? (long) call.argument(ARGUMENT_PERIOD_END)
-                : System.currentTimeMillis();
-        checkPermissionAndQueryImagesForPeriod(result,
-                activity, startPeriod, endPeriod);
+          ? (long) call.argument(ARGUMENT_PERIOD_END)
+          : System.currentTimeMillis();
+
+        checkPermissionAndQueryImagesForPeriod(result, activity, startPeriod, endPeriod);
         break;
       default:
         result.notImplemented();
@@ -76,68 +75,68 @@ public class FlutterGalleryPlugin implements MethodCallHandler {
 
   private void checkPermissionAndGetAllImages(final Result result, final Activity activity) {
     Dexter.withActivity(activity)
-            .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            .withListener(new PermissionListener() {
-              @Override
-              public void onPermissionGranted(PermissionGrantedResponse response) {
-                result.success(getAllImageList(activity));
-              }
+      .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+      .withListener(new PermissionListener() {
+        @Override
+        public void onPermissionGranted(PermissionGrantedResponse response) {
+          result.success(getAllImageList(activity));
+        }
 
-              @Override
-              public void onPermissionDenied(PermissionDeniedResponse response) {
+        @Override
+        public void onPermissionDenied(PermissionDeniedResponse response) {
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                builder.setMessage("This permission is needed for use this features of the app so please, allow it!");
-                builder.setTitle("We need this permission");
-                builder.setCancelable(false);
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                  public void onClick(DialogInterface dialog, int id) {
-                    dialog.cancel();
-                    Intent intent = new Intent();
-                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                    Uri uri = Uri.fromParts("package", activity.getPackageName(), null);
-                    intent.setData(uri);
-                    activity.startActivity(intent);
+          AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+          builder.setMessage("This permission is needed for use this features of the app so please, allow it!");
+          builder.setTitle("We need this permission");
+          builder.setCancelable(false);
+          builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+              dialog.cancel();
+              Intent intent = new Intent();
+              intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+              Uri uri = Uri.fromParts("package", activity.getPackageName(), null);
+              intent.setData(uri);
+              activity.startActivity(intent);
 
-                  }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                  public void onClick(DialogInterface dialog, int id) {
-                    result.error("Permission Denied", null, null);
-                    dialog.cancel();
-                  }
-                });
-                AlertDialog alert = builder.create();
-                alert.show();
+            }
+          });
+          builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+              result.error("Permission Denied", null, null);
+              dialog.cancel();
+            }
+          });
+          AlertDialog alert = builder.create();
+          alert.show();
 
 
-              }
+        }
 
-              @Override
-              public void onPermissionRationaleShouldBeShown(PermissionRequest permission, final PermissionToken token) {
+        @Override
+        public void onPermissionRationaleShouldBeShown(PermissionRequest permission, final PermissionToken token) {
 
-                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                builder.setMessage("This permission is needed for use this features of the app so please, allow it!");
-                builder.setTitle("We need this permission");
-                builder.setCancelable(false);
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                  public void onClick(DialogInterface dialog, int id) {
-                    dialog.cancel();
-                    token.continuePermissionRequest();
+          AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+          builder.setMessage("This permission is needed for use this features of the app so please, allow it!");
+          builder.setTitle("We need this permission");
+          builder.setCancelable(false);
+          builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+              dialog.cancel();
+              token.continuePermissionRequest();
 
-                  }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                  public void onClick(DialogInterface dialog, int id) {
-                    dialog.cancel();
-                    token.cancelPermissionRequest();
-                    result.error("Permission Denied", null, null);
-                  }
-                });
-                AlertDialog alert = builder.create();
-                alert.show();
-              }
-            }).check();
+            }
+          });
+          builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+              dialog.cancel();
+              token.cancelPermissionRequest();
+              result.error("Permission Denied", null, null);
+            }
+          });
+          AlertDialog alert = builder.create();
+          alert.show();
+        }
+      }).check();
 
   }
 
@@ -147,11 +146,13 @@ public class FlutterGalleryPlugin implements MethodCallHandler {
     ArrayList<String> allImageList = new ArrayList<>();
 
     Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-    String[] projection = {MediaStore.Images.ImageColumns.DATA,
-            MediaStore.Images.ImageColumns.DISPLAY_NAME,
-            MediaStore.Images.ImageColumns.DATE_ADDED,
-            MediaStore.Images.ImageColumns.TITLE};
-    Cursor c = activity.getContentResolver().query(uri, projection, null, null, null);
+    String[] projection = {
+      MediaStore.Images.ImageColumns.DATA,
+      MediaStore.Images.ImageColumns.DISPLAY_NAME,
+      MediaStore.Images.ImageColumns.DATE_ADDED,
+      MediaStore.Images.ImageColumns.TITLE
+    };
+    Cursor c = activity.getContentResolver().query(uri, projection,null, null, null);
     if (c != null) {
       while (c.moveToNext()) {
         Log.e("", "getAllImageList: " + c.getString(0));
@@ -167,23 +168,23 @@ public class FlutterGalleryPlugin implements MethodCallHandler {
                                                       final long startPeriod,
                                                       final long endPeriod) {
     Dexter.withActivity(activity)
-            .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            .withListener(new PermissionListener() {
-              @Override
-              public void onPermissionGranted(PermissionGrantedResponse response) {
-                result.success(getImagesForPeriod(activity, startPeriod, endPeriod));
-              }
+      .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+      .withListener(new PermissionListener() {
+        @Override
+        public void onPermissionGranted(PermissionGrantedResponse response) {
+          result.success(getImagesForPeriod(activity, startPeriod, endPeriod));
+        }
 
-              @Override
-              public void onPermissionDenied(PermissionDeniedResponse response) {
-                result.error("Permission Denied", null, null);
-              }
+        @Override
+        public void onPermissionDenied(PermissionDeniedResponse response) {
+          result.error("Permission Denied", null, null);
+        }
 
-              @Override
-              public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
-                result.error("Permission Denied", null, null);
-              }
-            }).check();
+        @Override
+        public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+          result.error("Permission Denied", null, null);
+        }
+      }).check();
   }
 
   private ArrayList<String> getImagesForPeriod(Activity activity,
